ad38b2bdae01b6ce331dbd5fa44c05a091d19154
iikypoh <muhtarmizi097@gmail.com>
2017-03-11 06:01:22 +0800

    Update safelink-3-multi.js

--- safelink-3-multi.js
+++ safelink-3-multi.js
@@ -25,7 +25,8 @@ if(x=="1"){xxx="go-to.html"}
 if(x=="2"){xxx="safe-your-link.html"}
 if(x=="3"){xxx="converter.html"}
 if(x=="4"){xxx="protec-link.html"}
-if(x=="5"){xxx="safe-link.html"}a_to_ve[i].href="https://safeindex.blogspot.com/2017/03/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="safe-link.html"}
+a_to_ve[i].href="https://safeindex.blogspot.com/2017/03/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
