4d8cf498c6ec88d037a9ad5398372cf68e6046b8
iikypoh <muhtarmizi097@gmail.com>
2017-03-15 12:30:13 +0800

    Update safelink-3-multi.js

--- safelink-3-multi.js
+++ safelink-3-multi.js
@@ -19,12 +19,13 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="go-to.html"}
 if(x=="2"){xxx="safe-your-link.html"}
 if(x=="3"){xxx="converter.html"}
 if(x=="4"){xxx="protec-link.html"}
+if(x=="5"){xxx="how-to-sign-out-of-gmail-apps.html"}
 a_to_ve[i].href="https://safeindex.blogspot.com/2017/03/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
